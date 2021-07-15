d50226b51965c6dbdb05d68f4dfa47dbb7976727
sushaan shetty <coolsushaan@gmail.com>
2021-07-15 22:47:15 +0530

    new section

---
+++ section4and5.py
@@ -0,0 +1,16 @@
+#comparison operators
+print(2.0==2)#T
+print('hello'=='Hello')#F
+print(2==2)#T
+print(3>=5)
+print(3<=5)
+print(3!=5)
+print(5!=5)
+print(5>10)
+print(5<10)
+print('chaining comparison operators - logical operators')
+print(1==1 and 'H'=='h')
+print(1==1 or 'H'=='h')
+print(not(1==1 or 'H'=='h'))
+print(1>2<3)#same as 1>2 and 2<3
+print(1>2 and 2<3)
